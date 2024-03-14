4f798ab6d70af260794465fc9e18d5d2a5d6e7fc
Coen <458416@student.fontys.nl>
2024-03-14 14:05:12 +0100

    Adding posting-service and tests

---
+++ posting-service/Controllers/PostController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace posting_service.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PostController : ControllerBase
+	{
+		// GET: api/<PostController>
+		[HttpGet]
+		public Models.PostModel Get()
+		{
+			Models.PostModel ExamplePost = new Models.PostModel
+			{
+				title = "Docker is a very spooky program",
+				Author = "Coen",
+				Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+				postdate = Convert.ToString(DateTime.Today),
+			};
+			return ExamplePost;
+		}
+		
+		// GET api/<PostController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<PostController>
+		[HttpPost]	
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<PostController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<PostController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ posting-service/Models/PostModel.cs
@@ -0,0 +1,10 @@
+﻿namespace posting_service.Models
+{
+	public class PostModel
+	{
+		public string title { get; set; }
+		public string Author { get; set; }
+		public string postdate { get; set; }
+		public string Description { get; set; }
+	}
+}
